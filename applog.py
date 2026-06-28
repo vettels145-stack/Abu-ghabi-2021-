fde4049c2486dd984d879fd41936956839c5c7ec
Seb Vettel <vettels145@gmail.com>
2026-06-28 09:12:33 +0700

    added all the updates and reworks in main, applogic

--- applog.py
+++ applog.py
@@ -4,19 +4,19 @@ import random
 def human_turn(sticks_left):
     while True:
         try:
-            take = int(input(f"Take matches from 1 t o3 only, my dear, there are only {sticks_left} matches left"))
-            if 1<=take<=min(3,stick_left):
+            take = int(input(f"Take matches from 1 to 3 only, my dear, there are only {sticks_left} matches left:  "))
+            if 1<=take<=min(3,sticks_left):
                 return take
             else:
-                print("You only can take from 1 to 3 matches at the time")
+                print("You only can take from 1 to 3 matches at the time ")
         except ValueError:
-            print("ERROR, type THE NUMBER")
+            print("ERROR, type THE NUMBER ")
 
 def comp_turn(sticks_left):
-    if sticks_left >= 4 and stick_left%4 != 0:
+    if sticks_left >= 4 and sticks_left%4 != 0:
         comp_take = sticks_left%4
     else:
-        comp_take = random.randint(1, min(3, stick_left))
-    print(f"Comp takes {comp_take} matches! Keep up)")
+        comp_take = random.randint(1, min(3, sticks_left))
+    print(f"Comp takes {comp_take} matches! Keep up) ")
     return comp_take
             
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from utils import winner_winner_chicken_dinner
+from applog import human_turn, comp_turn
+import time
+
 def start_game():
     sticks = 21
     turn = input("Wanna be first? (YES/NO) - ").strip().lower()
@@ -5,9 +9,27 @@ def start_game():
         human_first = True
     else:
         human_first = False
+        print("You were suppose to type yes or да, not some random syllables, ok?")
     play_game(human_first, sticks)
 
+def play_game(human_first, sticks):
+    current_pl = human_first
+    while sticks > 0:
+        if current_pl:
+            taken = human_turn(sticks)
+        else:
+            taken = comp_turn(sticks)
+        sticks -= taken
+        current_pl = not current_pl
+        if winner_winner_chicken_dinner(sticks):
+            winner = "YOU ARE THE WORLD CHAMPION! THE WORLD CHAMPION! MAX!" if current_pl else "Lewis won, history rewrited"
+            print(winner)
+            break
+if __name__ == "__main__":
+    import random
+    start_game()
 
+#time.sleep(10)
 
 
 
